7c12a6d989893c374147528de51adabe304d5b02
user-pc <kelle111@naver.com>
2020-10-01 09:58:42 +0900

    [02] evnet모듈 사용

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-var http = require("http");
-http.createServer(function(request, response){
-    // HTTP 헤더 전송 - HTTP Status , Content Type
-    response.writeHead(200, {'Content-Type' : 'text/pain'});
+// 이벤트 옵저버
+var events = require('events'); // node.js 내장 모듈
 
-    // Response Body에 보내는 내용
-    response.end("Hello World\n");
-}).listen(8081);
+// 이벤트 핸들러
+var eventEmitter = new events.EventEmitter();
 
-console.log("Server running at http://127.0.0.1:8001");
+// 1. 함수를 변수에 담아서 .on('이벤트이름', 함수) 로 바인딩한후 emit()으로 이벤트 실행
+var connectHandler = function connected() {
+    console.log("Connection Successful");
+    
+    // 이벤트 발생
+    eventEmitter.emit("data_recevied");
+}
+
+eventEmitter.on('connection', connectHandler);
+
+// 2. 함수를 콜백형태로 on()에 바로 전달
+eventEmitter.on('data_recevied', function(){
+    console.log("Data Received")
+})
+
+// 이벤트 발생
+eventEmitter.emit('connection');
+
+console.log("Program has ended")
