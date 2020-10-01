0166cff4043a30c8b754a94c06a633848c6bfcdd
user-pc <kelle111@naver.com>
2020-10-01 12:42:37 +0900

    [04] express 모듈 사용 no action

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
+var express = require('express')
+var app = express()
+var server = app.listen(3000, function() {
+    console.log("Express server has started on port 3000")
+});
 
-// 서버 생성
-http.createServer( function(request, response){
-    var pathname = url.parse(request.url).pathname;
+app.get('/', function(req, res){
+    res.send('Hello world');
+});
 
-    console.log("Request for "+ pathname + " received.");
-
-    // 파일 이름이 비어있으면 index.html로 자동 이동
-    if(pathname == "/") {
-        pathname = "/index.html";
-    }
-
-    // 파일 읽기
-    fs.readFile(pathname.substr(1), function(err, data){
-        if (err){
-            console.log(err);
-            response.writeHead(404, {'Content-Type':'text/html'});
-        } else {
-            response.writeHead(200, {'Content-Type' : 'text/html'});
-            response.write(data.toString());
-        }
-        response.end();
-    })
-}).listen(8081);
-
-
-console.log("Server running at http://127.0.0.1:8081/");
